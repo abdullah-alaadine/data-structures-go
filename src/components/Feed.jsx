d5a1bc16acfb23c1c2d07f98c4cbca5f396b6375
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-26 08:46:22 +0300

    feat: Implement API data fetching in Feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -11,8 +11,8 @@ const Feed = () => {
   useEffect(() => {
     setVideos(null);
 
-    // fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-      // .then((data) => setVideos(data.items))
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
     }, [selectedCategory]);
 
   return (
