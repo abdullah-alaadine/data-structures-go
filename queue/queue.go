257311926b804b8a40e9fefc22420fbad6fdbd9b
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-11-27 17:21:23 +0200

    create NewQueue function

--- queue/queue.go
+++ queue/queue.go
@@ -5,3 +5,9 @@ import linkedlist "github.com/abdullah-alaadine/data-structures-go/linked_list"
 type Queue struct {
 	list linkedlist.Linkedlist
 }
+
+func NewQueue() *Queue {
+	return &Queue{
+		list: *linkedlist.NewLinkedList(),
+	}
+}
