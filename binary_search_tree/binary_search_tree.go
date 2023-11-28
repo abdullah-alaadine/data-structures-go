f614e91dbf6d693efc6b7ca0931890f89c77b48b
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-11-28 06:14:44 +0200

    create (*BinarySearchTree).Insert

--- binary_search_tree/binary_search_tree.go
+++ binary_search_tree/binary_search_tree.go
@@ -13,3 +13,29 @@ type BinarySearchTree struct {
 func NewBinarySearchTree() *BinarySearchTree {
 	return &BinarySearchTree{}
 }
+
+func (t *BinarySearchTree) Insert(key int) {
+	newNode := &node{key: key}
+	if t.root == nil {
+		t.root = newNode
+		return
+	}
+	insertHelper(t.root, newNode)
+}
+
+func insertHelper(root *node, node *node) {
+	if root.key < node.key {
+		if root.right == nil {
+			root.right = node
+			return
+		}
+		insertHelper(root.right, node)
+	}
+	if root.key > node.key {
+		if root.left == nil {
+			root.left = node
+			return
+		}
+		insertHelper(root.left, node)
+	}
+}
