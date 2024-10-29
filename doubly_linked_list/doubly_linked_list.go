6a62b3fcacbe80e0862309dc92253ee7f4d755bc
knbr13 <abdullahalaadine63@gmail.com>
2024-10-30 01:11:45 +0200

    add PushBack func

--- doubly_linked_list/doubly_linked_list.go
+++ doubly_linked_list/doubly_linked_list.go
@@ -11,3 +11,15 @@ func New[T any]() *DoublyLinkedList[T] {
 }
 
 func (l *DoublyLinkedList[T]) Len() uint { return l.length }
+
+func (l *DoublyLinkedList[T]) PushBack(v T) {
+	n := NewNode(v)
+	if l.length == 0 {
+		l.head, l.tail = n, n
+	} else {
+		n.Prev = l.tail
+		l.tail.Next = n
+		l.tail = n
+	}
+	l.length++
+}
